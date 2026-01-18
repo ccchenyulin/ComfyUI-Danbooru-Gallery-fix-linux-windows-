b216cad40cd0f27ad07d777a4ce14b6a44859e8c
user7 <95221281+ccchenyulin@users.noreply.github.com>
2026-01-18 13:07:42 +0800

    修复linux启动comfyui后端Windows使用comfyui前端问题

--- py/model_name_extractor/model_name_extractor.py
+++ py/model_name_extractor/model_name_extractor.py
@@ -12,7 +12,9 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.abspath(os.path.join(current_dir, '..', '..'))
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
-from py.utils.logger import get_logger
+
+# 相对路径导入(mwh)
+from ..utils.logger import get_logger
 
 # 设置日志
 logger = get_logger(__name__)
